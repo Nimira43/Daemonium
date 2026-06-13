642ad0b55d932c4fb7966bb89b7ce7f8b8ed7265
Nimira43 <n.rabone72@aol.com>
2026-06-13 16:22:40 +0100

    POST request

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const port = 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Daemonium API'
